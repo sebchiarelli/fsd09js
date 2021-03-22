e42bb70d867a4ba563d1118f525b4b38c2cde8e5
remy3wa <remy.turpault@3wa.io>
2021-03-22 14:22:23 +0100

    corr ex02

--- cours/1-intro/exercices/02-tva.js
+++ cours/1-intro/exercices/02-tva.js
@@ -1,2 +1,6 @@
+function computeTTC(priceHT, tva = 20) {
+	return priceHT * (1 + tva / 100);
+}
+
 console.log(computeTTC(100)); // TVA de 20% par défaut, donc résultat : 120
-console.log(computeTTC(200, 5.5)); // TVA passée explicitement en 2è param, résulat: 210
+console.log(computeTTC(200, 5.5)); // TVA passée explicitement en 2è param, résulat: 211
