42fb6451893ef46d6a30a433d9b903646802eb95
remy3wa <remy.turpault@3wa.io>
2021-03-22 15:16:39 +0100

    code depart 04

---
+++ cours/1-intro/exercices/04-tva-x3.js
@@ -0,0 +1,6 @@
+function sumTTCPrices(prixHT1, prixHT2, prixHT3, TVA = 20) {}
+
+const pricesHT = [100, 200, 55];
+
+//console.log(sumTTCPrices()); // calculer la somme avec une TVA par défaut de 20%, res 426
+//console.log(); // avec une TVA de 5.5%, res 374,525
