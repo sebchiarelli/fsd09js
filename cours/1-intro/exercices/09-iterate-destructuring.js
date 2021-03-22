ba566d0c09625da8c5f7862de0497902d3eeabf7
remy3wa <remy.turpault@3wa.io>
2021-03-22 17:03:54 +0100

    code depart 09

---
+++ cours/1-intro/exercices/09-iterate-destructuring.js
@@ -0,0 +1,20 @@
+const students = [
+	{
+		name: "Alan",
+		family: {
+			mother: "Isa",
+			father: "Philippe",
+			sister: "Sylvie",
+		},
+		age: 35,
+	},
+	{
+		name: "Bernard",
+		family: {
+			mother: "Particia",
+			father: "Cécile",
+			sister: "Annie",
+		},
+		age: 55,
+	},
+];
