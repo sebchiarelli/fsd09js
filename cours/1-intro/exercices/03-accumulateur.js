8170f25bbd284449119dc5d6699fe3d5ca4601cf
remy3wa <remy.turpault@3wa.io>
2021-03-22 14:29:56 +0100

    code depart accumulator

---
+++ cours/1-intro/exercices/03-accumulateur.js
@@ -0,0 +1,12 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// retourne la première valeur du tableau en la supprimant du tableau
+// numbers.shift();
+
+function accumulator(numbers, acc = 0) {
+	// Une condition d'arrêt et retourner la somme des valeurs du tableau
+	// dans la fonction on ré-appelle la fonction elle-même
+	// accumulator(numbers, 10);
+}
+
+console.log(accumulator(numbers)); // doit retourner 55
