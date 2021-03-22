0abfd3e01cb53b1983972382a2aa24e0ab8ece1a
remy3wa <remy.turpault@3wa.io>
2021-03-22 14:31:41 +0100

    code depart 03

--- cours/1-intro/exercices/03-accumulateur.js
+++ cours/1-intro/exercices/03-accumulateur.js
@@ -4,8 +4,9 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // numbers.shift();
 
 function accumulator(numbers, acc = 0) {
+	// retourner la première valeur du tableau, la supprimer du tableau, l'additionner à l'accumulateur
 	// Une condition d'arrêt et retourner la somme des valeurs du tableau
-	// dans la fonction on ré-appelle la fonction elle-même
+	// dans la fonction on ré-appelle la fonction elle-même, en lui passant le nouveau tableau et la nouvelle valeur de l'accumulateur
 	// accumulator(numbers, 10);
 }
 
