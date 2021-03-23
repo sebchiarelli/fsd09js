b92a4384f95c3fa91a7e987e96e5d90b9edea9d0
remy3wa <remy.turpault@3wa.io>
2021-03-23 17:41:23 +0100

    corr 03

--- cours/2-poo/exercices/03-dragon-slayer.js
+++ cours/2-poo/exercices/03-dragon-slayer.js
@@ -5,6 +5,21 @@ class Player {
 		this._force = force;
 		this._shot = 0;
 	}
+	get name() {
+		return this._name;
+	}
+	computeDamage() {
+		return Math.round(100 * Math.random() * this._force) / 100;
+	}
+	decrementLife(dmg) {
+		this._life = Math.round(100 * (this._life - dmg)) / 100;
+	}
+	incrementShot() {
+		this._shot++;
+	}
+	isAlive() {
+		return this._life > 0;
+	}
 }
 
 class Dragon extends Player {}
@@ -16,7 +31,46 @@ class Game {
 		this._player1 = p1;
 		this._player2 = p2;
 	}
-	run() {}
+	run() {
+		while (this._player1.isAlive() && this._player2.isAlive()) {
+			const [assaillant, victim] =
+				Math.random() < 0.5
+					? [this._player1, this._player2]
+					: [this._player2, this._player1];
+			// l'assaillant détermine les points de dommage
+			const dmg = assaillant.computeDamage();
+			// on retire ce dommage des PV de la victime
+			victim.decrementLife(dmg);
+			// incrémenter le compteur de shots de l'assaillant
+			assaillant.incrementShot();
+			/*
+			if (Math.random() < 0.5) {
+				// l'assaillant détermine les points de dommage
+				const dmg = this._player1.computeDamage();
+				// on retire ce dommage des PV de la victime
+				this._player2.decrementLife(dmg);
+				// incrémenter le compteur de shots de l'assaillant
+				this._player1.incrementShot();
+			} else {
+				// l'assaillant détermine les points de dommage
+				const dmg = this._player2.computeDamage();
+				// on retire ce dommage des PV de la victime
+				this._player1.decrementLife(dmg);
+				// incrémenter le compteur de shots de l'assaillant
+				this._player2.incrementShot();
+			}
+      */
+		}
+		this.displayResults();
+	}
+	displayResults() {
+		console.log(
+			`Le vainqueur est ${
+				this._player1.isAlive() ? this._player1.name : this._player2.name
+			}`
+		);
+		console.log(this);
+	}
 }
 
 const knight = new Knight("Lancelot", 100, 10);
