d5aa07952a5b71319ddb1e71bd2bd46dcfe42a98
remy3wa <remy.turpault@3wa.io>
2021-03-24 12:38:33 +0100

    code depart

--- cours/3-async/exercices/04-json-dragon.js
+++ cours/3-async/exercices/04-json-dragon.js
@@ -1,12 +1,17 @@
 const fs = require("fs");
 
-fs.readFile("./data/dragons.json", { encoding: "utf8" }, (err, data) => {
-	// impossible de lire le fichier
-	if (err) {
-		console.log("File read failed:", err);
-		return;
-	}
-	// success
-	// JSON.parse permet de transformer un fichier JSON en un objet JSON JS
-	console.log("File data:", JSON.parse(data));
-});
+const getJsonFromFile = (path) =>
+	new Promise((resolve, reject) => {
+		setTimeout(() => {
+			// fs.readFile("./data/dragons.json", { encoding: "utf8" }, (err, data) => {
+			// 	// impossible de lire le fichier
+			// 	if (err) {
+			// 		console.log("File read failed:", err);
+			// 		return;
+			// 	}
+			// 	// success
+			// 	// JSON.parse permet de transformer un fichier JSON en un objet JSON JS
+			// 	console.log("File data:", JSON.parse(data));
+			// });
+		}, 1000);
+	});
