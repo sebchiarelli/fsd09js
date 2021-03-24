1762c67bae131ee270eba2ff84a193ade87f42f0
remy3wa <remy.turpault@3wa.io>
2021-03-24 15:05:22 +0100

    corr partielle 05

--- cours/3-async/exercices/05-relationships.js
+++ cours/3-async/exercices/05-relationships.js
@@ -11,7 +11,19 @@ const getJsonFromFile = (path) =>
 		});
 	});
 
-// TODO Faire une promesse globale qui est résolue quand on obtient les contenus désérialisés des 2 fichiers
+// Faire une promesse globale qui est résolue quand on obtient les contenus désérialisés des 2 fichiers
+Promise.all([
+	getJsonFromFile("./data/dragons.json"),
+	getJsonFromFile("./data/relationships.json"),
+])
+	// double destructuring : la valeur de résolution est un tableau à 2 éléments (les valeurs de résolution de chaque promesse de départ)
+	// ces 2 éléments sont eux-mêmes des objets dans lesquels une seule propriété nous intéresse (dragons dans le 1er, relationships dans le 2nd)
+	.then(([{ dragons }, { relationships }]) => {
+		console.log(dragons);
+		console.log(relationships);
+	})
+	.catch((err) => console.log(`ERREUR: ${err.message}`));
+
 // Après résolution, créer une structure de données contenant des relations nommées
 
 /* Exemple de structure souhaitée */
