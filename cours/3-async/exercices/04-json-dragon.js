e7aad0ce82ca6f827305ade6655a70f5e5291a17
remy3wa <remy.turpault@3wa.io>
2021-03-24 13:03:21 +0100

    fonction lecture désérialisation json

--- cours/3-async/exercices/04-json-dragon.js
+++ cours/3-async/exercices/04-json-dragon.js
@@ -3,15 +3,20 @@ const fs = require("fs");
 const getJsonFromFile = (path) =>
 	new Promise((resolve, reject) => {
 		setTimeout(() => {
-			// fs.readFile("./data/dragons.json", { encoding: "utf8" }, (err, data) => {
-			// 	// impossible de lire le fichier
-			// 	if (err) {
-			// 		console.log("File read failed:", err);
-			// 		return;
-			// 	}
-			// 	// success
-			// 	// JSON.parse permet de transformer un fichier JSON en un objet JSON JS
-			// 	console.log("File data:", JSON.parse(data));
-			// });
+			fs.readFile(path, { encoding: "utf8" }, (err, data) => {
+				// impossible de lire le fichier
+				if (err) {
+					reject(err);
+					return;
+				}
+				// success
+				// JSON.parse permet de transformer un fichier JSON en un objet JSON JS
+				//console.log("File data:", JSON.parse(data));
+				resolve(JSON.parse(data));
+			});
 		}, 1000);
 	});
+
+getJsonFromFile("./data/dragons.json")
+	.then((res) => console.log(res))
+	.catch((err) => console.log(`ERREUR: ${err.message}`));
