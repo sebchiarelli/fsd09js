eb34f843fdf237290b00d8a49eed5e69fe42b9f9
remy3wa <remy.turpault@3wa.io>
2021-03-25 11:56:21 +0100

    code depart 11

---
+++ cours/3-async/exercices/11-fetch.js
@@ -0,0 +1,13 @@
+const fetch = require("node-fetch");
+
+fetch("https://jsonplaceholder.typicode.com/users")
+	//.then((response) => response.text())
+	/*.then((jsonString) => {
+		const data = JSON.parse(jsonString);
+		console.log(data);
+	});*/
+	//.then((jsonString) => JSON.parse(jsonString))
+	.then((response) => response.json())
+	.then((users) => {
+		console.log(users);
+	});
