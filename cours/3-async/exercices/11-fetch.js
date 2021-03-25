9dafd3e5d77a24bf1ca33214601a8e231eef3322
remy3wa <remy.turpault@3wa.io>
2021-03-25 12:59:10 +0100

    corr 11

--- cours/3-async/exercices/11-fetch.js
+++ cours/3-async/exercices/11-fetch.js
@@ -1,13 +1,32 @@
+const fs = require("fs");
 const fetch = require("node-fetch");
 
 fetch("https://jsonplaceholder.typicode.com/users")
-	//.then((response) => response.text())
-	/*.then((jsonString) => {
-		const data = JSON.parse(jsonString);
-		console.log(data);
-	});*/
-	//.then((jsonString) => JSON.parse(jsonString))
 	.then((response) => response.json())
 	.then((users) => {
-		console.log(users);
+		/*
+    const userMap = new Map();
+		for (const {
+			name,
+			address: { geo },
+		} of users) {
+			userMap.set(name, geo);
+		}
+		console.log(userMap);
+    */
+		//const usersSlim = users.map(({ name, address: { geo } }) => [name, geo]);
+		const userMap = new Map(
+			users.map(({ name, address: { geo } }) => [name, geo])
+		);
+		console.log(userMap);
+
+		// transformer la Map en tableau pour la stringifier, utiliser de l'indentation pour que le string obtenu soit facilement lisible
+		const jsonString = JSON.stringify(Array.from(userMap), null, 2);
+		console.log(jsonString);
+
+		// écrire un fichier contenant ce string
+		fs.writeFile("./data/users.json", jsonString, (err) => {
+			if (err) throw err;
+			console.log("The file has been saved!");
+		});
 	});
