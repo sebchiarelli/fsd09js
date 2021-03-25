6ac2d70e9e479daa564f0de950736fc2438c98c0
remy3wa <remy.turpault@3wa.io>
2021-03-25 10:39:47 +0100

    corr partielle 09

--- cours/3-async/exercices/09-anim.js
+++ cours/3-async/exercices/09-anim.js
@@ -4,10 +4,21 @@
  * @returns la PROMESSE de la chaine de caractères
  */
 
-const anim = () => new Promise();
+const anim = () =>
+	new Promise((resolve) => {
+		setTimeout(() => {
+			const letters = "ABCDEFGHIJKLMNOPQRSTUVWX";
+			const digit = Math.ceil(Math.random() * 9);
+			const letter = letters[Math.floor(Math.random() * 24)];
+			resolve(`${digit}${letter}`);
+		}, 500);
+	});
 
 /**
  * Fonction asynchrone qui génère une secret key en appelant 10 fois de suite anim et en concaténant les résultats
+ * @returns la promesse de la clé secrete
  */
+const generateSecretKey = async () => {};
 
 // CP: générer la secret key
+generateSecretKey();
