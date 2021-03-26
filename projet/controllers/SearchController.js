c7ec64dc54f77cecab1416671226d567e30b31e5
remy3wa <remy.turpault@3wa.io>
2021-03-26 09:50:13 +0100

    formulaire search

--- projet/controllers/SearchController.js
+++ projet/controllers/SearchController.js
@@ -3,8 +3,21 @@ class SearchController {
 		this.viewPath = "views/search.html";
 	}
 
+	fillYears() {
+		// Generer les balises option en fonction de l'année courante
+		const currentYear = new Date().getFullYear();
+		const options = ["<option>-- Choisir --</option>"];
+		for (let y = currentYear + 1; y >= currentYear - 2; y--) {
+			options.push(`<option value="${y}">${y}</option>`);
+		}
+		// MAJ le select
+		document.querySelector("#date_start").innerHTML = options.join("");
+	}
+
 	executeAfterDomUpdate() {
 		console.log("Search OK");
+		// Remplir la liste déroulante des années
+		this.fillYears();
 	}
 }
 
