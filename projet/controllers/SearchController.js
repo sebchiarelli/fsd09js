e97808b8a122bdf9a207dc4fa50521f711c02aac
remy3wa <remy.turpault@3wa.io>
2021-03-26 10:04:37 +0100

    extraction données form

--- projet/controllers/SearchController.js
+++ projet/controllers/SearchController.js
@@ -14,10 +14,28 @@ class SearchController {
 		document.querySelector("#date_start").innerHTML = options.join("");
 	}
 
+	/* Cette méthode est appelée en callback de addEventListener, ce qui pourrait modifier la signification de this.
+	   C'est pour garder le this du contexte parent (this = instance du controleur, dans la méthode executeAfterDomUpdate
+		 où l'appel est fait) qu'on écrit onSubmitForm sous forme de fonction fléchée.
+		 Seule différence fonctionnelle entre une fonction classique et une fonction fléchée : la fonction fléchée ne
+		 modifie jamais la valeur de this.
+	*/
+	onSubmitForm = (e) => {
+		e.preventDefault();
+		// récupérer les valeurs saisies dans le formulaire
+		const q = document.querySelector("#q").value;
+		const date_start = document.querySelector("#date_start").value;
+		const sort = document.querySelector("#sort").value;
+		console.log(q, date_start, sort);
+	};
+
 	executeAfterDomUpdate() {
-		console.log("Search OK");
 		// Remplir la liste déroulante des années
 		this.fillYears();
+		// Surveiller le submit du formulaire
+		document
+			.querySelector("#searchForm")
+			.addEventListener("submit", this.onSubmitForm);
 	}
 }
 
